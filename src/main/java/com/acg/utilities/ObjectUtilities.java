dfdf3fabb9331adfcaaf4386883d203380d34ad3
Shagun Bakliwal <shagunbakliwal@gmail.com>
2018-09-12 00:32:43 +0530

    exclusions varargs

--- src/main/java/com/acg/utilities/ObjectUtilities.java
+++ src/main/java/com/acg/utilities/ObjectUtilities.java
@@ -4,19 +4,19 @@ import java.lang.reflect.Field;
 
 public class ObjectUtilities {
 
-	public static boolean isNull(Object object) {
+	public static boolean isNull(Object object, Object... exclusions) {
 		Field fields[] = object.getClass().getDeclaredFields();
+
 		for (Field field : fields) {
 			try {
-				if (field.get(object) == null) {
-					return false;
+				if (field.get(field) == null) {
+					return true;
 				}
 			} catch (IllegalArgumentException e) {
 				return false;
 			} catch (IllegalAccessException e) {
 				return false;
 			}
-			return true;
 		}
 		return false;
 	}
