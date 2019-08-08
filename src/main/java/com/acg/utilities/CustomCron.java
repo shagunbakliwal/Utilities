0ac23a38d90a68d9c2ab13701e9e7ee2f0fc3480
Shagun Bakliwal <shagunbakliwal@gmail.com>
2019-08-08 14:41:49 +0530

    Update CustomCron.java

--- src/main/java/com/acg/utilities/CustomCron.java
+++ src/main/java/com/acg/utilities/CustomCron.java
@@ -20,9 +20,8 @@ public class CustomCron implements SchedulingConfigurer {
 		long interval = 3000;
 		long initialDelay = 5000;
 		taskRegistrar.addCronTask(new CronTask(runnable, new CronTrigger("30 8 14 * * *", TimeZone.getDefault())));// 2:08:30pm
-		taskRegistrar.addFixedRateTask(new IntervalTask(runnable, interval, initialDelay)); // 5 sec initial delay as
-																							// soon as app is up, then
-																							// trigger job in 0/3secs.
+		// 5 sec initial delay as soon as app is up, then trigger job in 0/3secs.
+		taskRegistrar.addFixedRateTask(new IntervalTask(runnable, interval, initialDelay)); 		
 		//2019-08-08 14:13:23.192  INFO 11128 --- [           main] com.example.demo.AsyncDemoApplication    : Started AsyncDemoApplication in 3.154 seconds (JVM running for 4.531)
 		//2019-08-08 14:13:28.129  INFO 11128 --- [lTaskScheduler1] com.example.demo.scheduler.CustomTask    : Started executing custom task in thread ThreadPoolTaskScheduler1
 	}
